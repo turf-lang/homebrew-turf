d46cefa41b4cc517456539dfd00469ef2a2925bb
Aayush Shukla <me@a2ys.dev>
2026-03-05 14:51:52 +0530

    chore: bump version to v0.7.0 from v0.6.0 (@a2ys)
    Signed-off-by: Aayush Shukla <me@a2ys.dev>

--- turf.rb
+++ turf.rb
@@ -1,14 +1,14 @@
 class Turf < Formula
   desc "Turf compiler"
   homepage "https://github.com/turf-lang/turf-lang"
-  version "v0.6.0"
+  version "v0.7.0"
 
   if OS.mac?
     url "https://github.com/turf-lang/turf-lang/releases/download/#{version}/turf-#{version}-macos-arm64.tar.gz"
-    sha256 "53a8fade084434f437d7d0f9ed04011cbe97c3094857a1aaed58eba01dfdf681"
+    sha256 "e79f78939aa247257e981777af52ff48a3a88271240fa37d264851cb6fefaa1f"
   elsif OS.linux?
     url "https://github.com/turf-lang/turf-lang/releases/download/#{version}/turf-#{version}-linux-x86_64.tar.gz"
-    sha256 "bf73bba242513f91c77edd98dc5a756042ca49fd377c6b4c6e9bb5c70b8f9dff"
+    sha256 "a79ec5b207ca1657ea0aa3cf87186cce58d40dc89a8a86263030d302ba722da8"
   end
 
   def install
